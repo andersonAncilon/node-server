4ecbe825c28d7ff79d1675008f0dcc5de552bd22
anderson.santos@tre-to.jus.br <anderson.santos@tre-to.jus.br>
2018-10-02 17:30:26 -0300

    Aula 26.

--- test/create_test.js
+++ test/create_test.js
@@ -2,8 +2,13 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('Creating user records', () => {
-    it('saves a user', () => {
+    it('saves a user', (done) => {
         const joe = new User({ name: 'Joe' })
         joe.save()
+            .then(() => {
+                //Has Joe been saved succefully?
+                assert(!joe.isNew)
+                done()
+            })
     })
 });
